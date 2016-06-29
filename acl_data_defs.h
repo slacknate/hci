b16980838197a48121fc06d61dce679a9f0eaa35
Nate Duarte <slacknate@gmail.co>
2016-06-28 23:16:40 -0400

    [hci] use proper hex constants for flag constant vars

--- acl_data_defs.h
+++ acl_data_defs.h
@@ -8,15 +8,15 @@
  */
 const uint8_t PB_FLAG_0 = 0x00;
 const uint8_t PB_FLAG_1 = 0x01;
-const uint8_t PB_FLAG_2 = 0x10;
-const uint8_t PB_FLAG_3 = 0x11;
+const uint8_t PB_FLAG_2 = 0x02;
+const uint8_t PB_FLAG_3 = 0x03;
 
 /*
  * TODO: give these real names?
  */
 const uint8_t BC_FLAG_0 = 0x00;
 const uint8_t BC_FLAG_1 = 0x01;
-const uint8_t BC_FLAG_2 = 0x10;
-const uint8_t BC_FLAG_3 = 0x11;
+const uint8_t BC_FLAG_2 = 0x02;
+const uint8_t BC_FLAG_3 = 0x03;
 
 #endif //HCI_ACL_DATA_DEFS_H
