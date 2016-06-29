2cf56323a51a25f7a40c1bf7a4783b6508ae3bfb
Nate Duarte <slacknate@gmail.co>
2016-06-29 18:21:06 -0400

    [hci] remove unnecessary header

--- cmd_pkt.h
+++ cmd_pkt.h
@@ -3,7 +3,29 @@
 
 #include <cstdint>
 #include <cstdlib>
-#include "cmd_defs.h"
+
+/*
+ * OpCode Group Field constants.
+ */
+const uint16_t OGF_NOOP      = 0x00;
+const uint16_t OGF_LINK_CTL  = 0x01;
+const uint16_t OGF_LINK_POL  = 0x02;
+const uint16_t OGF_HC_BB     = 0x03;
+const uint16_t OGF_INFO      = 0x04;
+const uint16_t OGF_STATUS    = 0x05;
+const uint16_t OGF_TESTING   = 0x06;
+
+const uint16_t OGF_LOGO_TEST = 0x3E;
+const uint16_t OGF_VND_COMM  = 0x3F;
+
+
+/*
+ * OpCode Command Field constants.
+ */
+const uint16_t OCF_NOOP = 0x0000;
+/*
+ * TODO: finish implementing!
+ */
 
 struct hci_cmd_pkt {
 
